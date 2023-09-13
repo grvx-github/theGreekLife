47ad6a819a8a3a48bfb3d851dadced07d60022b0
Gaurav Jha <104722784+grvx-github@users.noreply.github.com>
2023-09-13 13:02:24 +0530

    Add files via upload

--- src/app/holiday/[id]/page.js
+++ src/app/holiday/[id]/page.js
@@ -1,73 +1,55 @@
-'use client'
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import mongoose from 'mongoose';
-import { useRouter } from 'next/navigation';
-import Holiday from '/models/Holiday';
+import Holiday from '../../../../models/Holiday';
 import HolidayComp from '@/app/components/HolidayComp';
 import dbConnect from '@/app/middlewares/mongoose';
 
 const HolidayId = (props) => {
-	const router = useRouter();
-	
-	console.log(props);
+  const router = useRouter();
+  const [holidayData, setHolidayData] = useState(props.holidayData);
+  const [error, setError] = useState(props.error);
 
-	return (
-		<div>
-			<HolidayComp props={props} />
-			
-		</div>
-	);
+  useEffect(() => {
+    const fetchData = async () => {
+      const id = router.query.id;
+
+      // Check if there's an existing database connection
+      if (!mongoose.connections[0].readyState) {
+        await mongoose.connect('mongodb://127.0.0.1:27017/', {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          dbName: 'greekLifeDB'
+        });
+      }
+
+      try {
+        const holidayData = await Holiday.findOne({ _id: id });
+
+        if (!holidayData) {
+          console.error(`Holiday data not found for ID: ${id}`);
+          setError('Holiday data not found');
+        } else {
+          const serializedData = JSON.parse(JSON.stringify(holidayData));
+          setHolidayData(serializedData);
+        }
+      } catch (error) {
+        console.error('Error fetching holiday data:', error);
+        setError('Failed to fetch holiday data');
+      }
+    };
+
+    fetchData();
+  }, [router.query.id]);
+
+  return (
+    <div>
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <HolidayComp holidayData={holidayData} />
+      )}
+    </div>
+  );
 };
 
 export default HolidayId;
-
-export async function getServerSideProps(context) {
-	const id = context.query.id;
-
-	// Check if there's an existing database connection
-	if (!mongoose.connections[0].readyState) {
-		await mongoose.connect('mongodb://127.0.0.1:27017/', {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			dbName: 'greekLifeDB'
-		});
-	}
-
-	console.log(id);
-
-	try {
-		const holidayData = await Holiday.findOne({ _id: id });
-		console.log(holidayData);
-
-		if (!holidayData) {
-			console.error(`Holiday data not found for ID: ${id}`);
-			return {
-				props: {
-					error: 'Holiday data not found',
-				},
-			}}
-
-			const serializedData = JSON.parse(JSON.stringify(holidayData));
-
-
-		// Return the serialized data as props
-		return {
-			props: {
-				holidayData: serializedData,
-			},
-		};		
-		
-	} catch (error) {
-		console.error('Error fetching holiday data:', error);
-		// Handle the error or return an error message as needed
-		return {
-			props: {
-				error: 'Failed to fetch holiday data',
-			},
-		};
-	}
-	
-}
-
-
